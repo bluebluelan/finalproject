6c5e3e99c407fd73ea8683b28f99e3f9a60a7da5
blueMac <bluebluebluelan@gmail.com>
2016-04-08 16:36:53 +0900

    finalized single thread validator

---
+++ sudokuvalidator.c
@@ -0,0 +1,110 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+#define LENGTH 9
+#define BLOCK_LENGTH 3
+
+static int *puz[LENGTH];
+
+
+int main(int argc, const char *argv[]) {
+	
+	int i, j, k, l;
+
+	if (argc < 2) {
+		printf("no input\n");
+		return 0;
+	}
+	for (i = 0; i < LENGTH; i++)
+		puz[i] = (int *)malloc(sizeof(int) * LENGTH);
+	
+	FILE *file;
+	
+	if ((file = fopen(argv[1], "r")))
+	{
+
+		for (i = 0; i < LENGTH; i++) {
+
+			int *ptr = puz[i];
+			fscanf(file, "%d %d %d %d %d %d %d %d %d", ptr, ptr+1, ptr+2, ptr+3, ptr+4, ptr+5, ptr+6, ptr+7, ptr+8);
+		}
+		printf("File Parsed.\n");
+	
+		fclose(file);
+	}
+
+	int val1, val2;
+
+	for (k = 0;k< LENGTH; k++){
+		for (i = 0; i < LENGTH; i++) {
+				val1 = puz[k][i];
+				for (j = 0; j < LENGTH; j++) {
+					val2 = puz[k][j];
+					if (val1 == val2) {		
+						if (!(i == j))
+						{
+							printf("we found duplicate in [%d][%d][%d][%d]  \n", k, i, k, j);
+							return 0;					
+						}
+				}
+			}
+		}
+	}
+	printf("column down\n");
+
+	for (k = 0;k< LENGTH; k++){
+		for (i = 0; i < LENGTH; i++) {
+				val1 = puz[i][k];
+				for (j = 0; j < LENGTH; j++) {
+					val2 = puz[j][k];
+					if (val1 == val2) {		
+						if (!(i == j))
+						{
+							printf("we found duplicate in [%d][%d][%d][%d]  \n", i, k, i, k);
+							return 0;
+					}
+				}
+			}
+		}
+	}
+
+	printf("row down\n");
+
+	for (k = 0; k < BLOCK_LENGTH; k++){
+		for (l = 0; l < BLOCK_LENGTH; l++){
+			int clonearr[9]={1,2,3,4,5,6,7,8,9};
+			val1=puz[k*3][l*3];
+			if(clonearr[val1-1]!=0){
+				clonearr[val1-1]=0;
+			}
+			for (i = 0; i < BLOCK_LENGTH; i++){
+				for (j = 0; j < BLOCK_LENGTH; j++){
+					val2=puz[k*3+i][l*3+j];
+					if(clonearr[val2-1]!=0){
+						clonearr[val2-1]=0;
+					}
+					else if(clonearr[val2-1]==0 && i+j != 0){
+
+						printf("we found duplicate in [%d][%d]\n", k*3+i, l*3+j);
+						return 0;
+					}
+
+				}
+			}
+		}
+	}
+			
+	printf("block down\n");
+
+	
+ 	/* Free memory for the columns in each row */
+ 	for (i = 0; i < LENGTH; i++)
+ 		free(puz[i]);
+	
+	printf("done\n");
+	// return with success status
+	return 0;
+}
